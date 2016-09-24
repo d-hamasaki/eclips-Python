872df33331ef3e2e868e1f23f32257d713295571
DaisukeHamasaki <ds.ham83@gmail.com>
2016-09-24 17:32:38 +0900

    Python　どこがわかりやすいんだかサッパリ

--- Python-ref/HelloEgg.py
+++ Python-ref/HelloEgg.py
@@ -1,8 +1,20 @@
+# -*- coding: utf-8 -*-
+
 '''
 Created on 2016/09/24
 
 @author: daisuke
 '''
 
+
+
+
+print ('modul is Load')
+ 
+def test():
+    print ('関数：testを呼び出しました')
+    
 if __name__ == '__main__':
-    print('Hello Egg!!')
+    print ('python-izm')
+#   print 'パイソンイズム'
+    test()
